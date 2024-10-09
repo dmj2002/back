1e051555c89116cbb759c18c59b63893b964427d
Ding mengjie <D=473332451@qq.com>
2024-10-09 21:34:44 +0800

    换成mybatis-plus

--- src/main/java/com/hust/ewsystem/usermanage/entity/User.java
+++ src/main/java/com/hust/ewsystem/usermanage/entity/User.java
@@ -1,7 +1,6 @@
 package com.hust.ewsystem.usermanage.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,7 +15,6 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("user")
 public class User extends Model<User> implements Serializable {
 
     private static final long serialVersionUID = 1L;
