cbaa098571ff8cbd7d2e3bb74c80888da3483396
Ding mengjie <D=473332451@qq.com>
2024-10-28 17:09:28 +0800

    异步训练模型

--- src/main/java/com/hust/ewsystem/service/impl/ModelsServiceImpl.java
+++ src/main/java/com/hust/ewsystem/service/impl/ModelsServiceImpl.java
@@ -6,7 +6,6 @@ import com.hust.ewsystem.entity.Models;
 import com.hust.ewsystem.mapper.ModelsMapper;
 import com.hust.ewsystem.service.ModelsService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
