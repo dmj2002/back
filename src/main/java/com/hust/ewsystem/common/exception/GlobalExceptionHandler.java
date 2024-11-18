05738eec0dae05a3ce052adc6fc3b0b432e3188f
Ding mengjie <D=473332451@qq.com>
2024-11-18 10:52:37 +0800

    model and warning getlist

--- src/main/java/com/hust/ewsystem/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hust/ewsystem/common/exception/GlobalExceptionHandler.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
@@ -36,6 +38,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<EwsResult<?>> handleFileSaveException(CrudException e) {
         e.printStackTrace();
         EwsResult<?> result = EwsResult.error(StringUtils.hasLength(e.getMessage()) ? e.getMessage() : "数据库操作失败");
+        return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED); // 401 状态码
+    }
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<EwsResult<?>> handleMissingParams(MissingServletRequestParameterException e) {
+        e.printStackTrace();
+        EwsResult<?> result = EwsResult.error("缺少必需的参数: " +  e.getParameterName());
         return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED); // 401 状态码
     }
 }
