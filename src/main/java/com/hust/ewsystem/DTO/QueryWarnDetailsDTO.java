3cd7e89d36e6dafd0ddce061b88dec44be171989
xudayang <xudayang@123.com>
2024-11-22 16:57:41 +0800

    feat: 预警详情测点分析趋势图后台接口

---
+++ src/main/java/com/hust/ewsystem/DTO/QueryWarnDetailsDTO.java
@@ -0,0 +1,49 @@
+package com.hust.ewsystem.DTO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @BelongsProject: back
+ * @BelongsPackage: com.hust.ewsystem.DTO
+ * @Author: xdy
+ * @CreateTime: 2024-11-22  09:54
+ * @Description: 查询预警详情DTO
+ * @Version: 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class QueryWarnDetailsDTO {
+
+    /**
+     * 真实测点ID
+     */
+    @NotNull(message = "模型ID不能为空")
+    private Integer modelId;
+
+    /**
+     * 风机ID
+     */
+    @NotNull(message = "风机ID不能为空")
+    private Integer turbineId;
+
+    /**
+     * 开始时间 yyyy-MM-dd HH:mm:ss
+     */
+    @NotNull(message = "开始时间不能为空")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date startDate;
+
+    /**
+     * 结束时间 yyyy-MM-dd HH:mm:ss
+     */
+    @NotNull(message = "结束时间不能为空")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date endDate;
+}
---
+++ src/main/java/com/hust/ewsystem/service/impl/RealPortServiceImpl.java
@@ -0,0 +1,56 @@
+package com.hust.ewsystem.service.impl;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hust.ewsystem.DTO.QueryWarnDetailsDTO;
+import com.hust.ewsystem.common.constant.CommonConstant;
+import com.hust.ewsystem.common.util.DateUtil;
+import com.hust.ewsystem.entity.CommonData;
+import com.hust.ewsystem.entity.RealPoint;
+import com.hust.ewsystem.entity.RealPointValue;
+import com.hust.ewsystem.mapper.CommonDataMapper;
+import com.hust.ewsystem.mapper.RealPointValueMapper;
+import com.hust.ewsystem.mapper.RealPortMapper;
+import com.hust.ewsystem.service.RealPortService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @BelongsProject: back
+ * @BelongsPackage: com.hust.ewsystem.service.impl
+ * @Author: xdy
+ * @CreateTime: 2024-11-22  11:03
+ * @Description:
+ * @Version: 1.0
+ */
+@Service
+@Transactional
+public class RealPortServiceImpl extends ServiceImpl<RealPortMapper, RealPoint> implements RealPortService {
+
+    @Resource
+    private CommonDataMapper commonDataMapper;
+
+    @Override
+    @DS("slave")
+    public Map<Integer,List<CommonData>> getRealPointValueList(List<Map<Integer, String>> pointLabels, QueryWarnDetailsDTO queryWarnDetailsDTO) {
+        List<CommonData> valueList;
+        Map<Integer,List<CommonData>> result = new LinkedHashMap<>();
+        String startDate = DateUtil.dateTimeToDateString(queryWarnDetailsDTO.getStartDate(), CommonConstant.DATETIME_FORMAT_1);
+        String endDate = DateUtil.dateTimeToDateString(queryWarnDetailsDTO.getEndDate(), CommonConstant.DATETIME_FORMAT_1);
+        for (Map<Integer, String> pointLabel : pointLabels) {
+            for (Map.Entry<Integer, String> entry : pointLabel.entrySet()) {
+                valueList = commonDataMapper.selectDataByTime(entry.getValue().toLowerCase(), startDate, endDate);
+                result.put(entry.getKey(),valueList);
+            }
+        }
+        return result;
+    }
+}
