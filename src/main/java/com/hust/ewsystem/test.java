e9ce21450875e2d21712ac5d3cf55c35c0086737
Ding mengjie <D=473332451@qq.com>
2024-12-30 11:10:52 +0800

    预警信息处理接口,完善挂起逻辑

--- src/main/java/com/hust/ewsystem/test.java
+++ src/main/java/com/hust/ewsystem/test.java
@@ -1,6 +1,8 @@
 package com.hust.ewsystem;
 
 
+import com.hust.ewsystem.entity.Reports;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -11,25 +13,11 @@ import java.util.concurrent.TimeUnit;
 public class test {
 
     public static void main(String[] args) {
-        // 创建一个带有 3 个线程的调度线程池
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(3);
+        Reports newReport = Reports.builder()
+                .reportId(1)
+                .status(2)
+                .build();
+        System.out.println(newReport);
 
-        // 定义三个不同的任务
-        List<Integer> a = new ArrayList<>();
-        a.add(2);
-        a.add(3);
-        a.add(5);
-        Runnable task1 = () -> System.out.println("Task 1 executed at " + System.currentTimeMillis());
-        Runnable task2 = () -> System.out.println("Task 2 executed at " + System.currentTimeMillis());
-        Runnable task3 = () -> System.out.println("Task 3 executed at " + System.currentTimeMillis());
-
-        // 每隔 2 秒执行一次 Task 1
-        ScheduledFuture<?> task1Future = scheduler.scheduleAtFixedRate(task1, 0, a.get(0), TimeUnit.SECONDS);
-        // 每隔 3 秒执行一次 Task 2
-        ScheduledFuture<?> task2Future = scheduler.scheduleAtFixedRate(task2, 0, a.get(1), TimeUnit.SECONDS);
-
-        // 每隔 5 秒执行一次 Task 3
-         scheduler.scheduleAtFixedRate(task3, 0, a.get(2), TimeUnit.SECONDS);
-         return ;
     }
 }
