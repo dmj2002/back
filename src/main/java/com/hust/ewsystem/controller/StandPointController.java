edf4df7414a8cee6237785f9975419a2733afca2
dmjkk <1966423145@qq.com>
2025-04-23 09:38:25 +0800

    剩余接口开发

--- src/main/java/com/hust/ewsystem/controller/StandPointController.java
+++ src/main/java/com/hust/ewsystem/controller/StandPointController.java
@@ -9,7 +9,7 @@ import com.hust.ewsystem.service.StandPointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
